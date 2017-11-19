be70c78de7033dd3a1ae1bdbbc20010edbf673b7
Renju Radhakrishnan <renju467@gmail.com>
2017-11-19 14:23:50 -0500

    completed endpoints

--- source/RewardsService/rewardsservice/handlers/customers_handler.py
+++ source/RewardsService/rewardsservice/handlers/customers_handler.py
@@ -17,6 +17,17 @@ class CustomerHandler(tornado.web.RequestHandler):
         customers = list(db.customers.find({}, {"_id": 0}))
         self.write(json.dumps(customers, indent = 4))
 
+    @coroutine
+    def post(self):
+        self.set_header('Content-Type', 'application/javascript')
+        customer = json.loads(self.request.body.decode('utf-8'))
+        client = MongoClient("mongodb", 27017)
+        db = client["customers"]
+        customer = db.customers.find_one({"email": str(customer['email'])})
+        customer.pop('_id')
+        self.write(json.dumps(customer, indent = 4))
+
+
 class PurchaseHandler(tornado.web.RequestHandler):
 
     def data_received(self, chunk):
@@ -46,40 +57,35 @@ class PurchaseHandler(tornado.web.RequestHandler):
         rewards_tier = ''
         rewards_name = ''
 
-        # New Customers
-        if customer is None:
-            for idx, reward in enumerate(rewards):
-                if idx < totalRewards:
-                    if points >= rewards[idx]['points'] and points < rewards[idx + 1]['points']:
-                        rewards_tier = reward['tier']
-                        rewards_name = reward['rewardName']
-                        next_rewards_tier = rewards[idx + 1]['tier']
-                        next_reward_name = rewards[idx + 1]['rewardName']
-                        next_rewards_tier_progress = round(1 - (rewards[idx + 1]['points'] - points ) / 100, 2)
-                elif idx is totalRewards:
+        if customer is not None:
+            points+=customer['points']
+
+        for idx, reward in enumerate(rewards):
+            if idx < totalRewards:
+                if points >= rewards[idx]['points'] and points < rewards[idx + 1]['points']:
                     rewards_tier = reward['tier']
                     rewards_name = reward['rewardName']
+                    next_rewards_tier = rewards[idx + 1]['tier']
+                    next_reward_name = rewards[idx + 1]['rewardName']
+                    next_rewards_tier_progress = round(1 - (rewards[idx + 1]['points'] - points) / 100, 2)
+            elif idx is totalRewards:
+                rewards_tier = reward['tier']
+                rewards_name = reward['rewardName']
 
-            new_customer = {
-                'email': email,
-                'points': points,
-                'rewards_tier': rewards_tier,
-                'rewards_name': rewards_name,
-                'next_rewards_tier': next_rewards_tier,
-                'next_rewards_name': next_reward_name,
-                'next_rewards_progress': next_rewards_tier_progress
-            }
-            print ('no email associated, creating new account')
+        updated_customer = {
+            'email': email,
+            'points': points,
+            'rewards_tier': rewards_tier,
+            'rewards_name': rewards_name,
+            'next_rewards_tier': next_rewards_tier,
+            'next_rewards_name': next_reward_name,
+            'next_rewards_progress': next_rewards_tier_progress
+        }
+
+        if customer is None:
+            db.customers.insert(updated_customer)
+            updated_customer.pop('_id')
         else:
-            print ('reached if statement')
-            print (customer)
-
-
-        # customer = list(db.customers.find({"email": str(customer['email'])}))
-        if new_customer is not None:
-            print('new customer' + json.dumps(new_customer))
-            self.write(json.dumps(new_customer, indent=4))
-            db.customers.insert(new_customer)
-            return
-        customer.pop('_id')
-        self.write(json.dumps(customer, indent=4))
+            db.customers.update_one({'_id': customer['_id']}, {'$set': updated_customer}, upsert=True)
+            customer.pop('_id')
+        self.write(json.dumps(updated_customer, indent=4))
--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,7 +1,8 @@
 from handlers.rewards_handler import RewardsHandler
 from handlers.customers_handler import PurchaseHandler
-
+from handlers.customers_handler import  CustomerHandler
 url_patterns = [
     (r'/rewards', RewardsHandler),
-    (r'/purchase', PurchaseHandler)
+    (r'/purchase', PurchaseHandler),
+    (r'/customers', CustomerHandler)
 ]
